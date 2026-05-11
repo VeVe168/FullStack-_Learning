d5d883f1e5304b70eb87f156accfce5ee5c555a5
Su <su@gmail.com>
2026-05-11 11:16:39 -0800

    ...

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
 const app = express();
+app.use(
+  cors({
+    origin: "http://your-frontend-domain.com", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  }),
+);
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
