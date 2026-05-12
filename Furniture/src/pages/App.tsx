cfc1b8437bd2c1df0b93a784b46a1123fd675907
Su <su@gmail.com>
2026-05-11 19:44:46 -0800

    ...

--- Furniture/src/pages/App.tsx
+++ Furniture/src/pages/App.tsx
@@ -1,29 +1,29 @@
 import { Button } from "@/components/ui/button";
-import { signUp, signIn } from "@/lib/auth-client";
+import { useSession, signOut } from "@/lib/auth-client";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 function App() {
-  const handleSignUp = async () => {
-    const name = "John Doe";
-    const email = "johndoe@gmail.com";
-    const password = "Password123";
-
-    const { data, error } = await signUp.email({ name, email, password });
-    console.log("data", data);
-    console.log("error", error);
-  };
-  const handleSignIn = async () => {
-    const email = "johndoe@gmail.com";
-    const password = "Password123";
-
-    const { data, error } = await signIn.email({ email, password });
-    console.log("data", data);
-    console.log("error", error);
-  };
-
+  const navigate = useNavigate();
+  const { data: session, isPending } = useSession();
+  useEffect(() => {
+    if (!isPending && !session) {
+      navigate("/login");
+    }
+  }, [session, isPending, navigate]);
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <h1>Welcome to fullstack development!</h1>
-      <Button onClick={handleSignUp}> Sign Up </Button>
-      <Button onClick={handleSignIn}> Sing In</Button>
+      {session && <p>Hello, {session.user.name}!</p>}
+      {session && <p>Your email: {session.user.email}</p>}
+      {session ? (
+        <Button onClick={() => signOut()}>Sign Out</Button>
+      ) : (
+        <p>Please sign in to access your account.</p>
+      )}
     </>
   );
 }
